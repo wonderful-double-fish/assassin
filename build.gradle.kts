e7c36a48b06cb11a65db3dac70fc899f85ca7b7a
dengchaojun996 <cjd0655@gmail.com>
2026-09-10 14:58:41 +0800

    chore(build): 添加项目构建任务并优化Gradle配置
    - 注册了名为 literalAssassin 的自定义构建任务
    - 在构建任务中添加了前置后置操作打印信息
    - 注释掉了 gradle.properties 中的调试缓存配置
    - 移除了 libs.versions.toml 中的注释说明内容
    - 简化了依赖管理和Spring Boot插件的注释说明

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,3 +37,15 @@ subprojects {
         }
     }
 }
+
+// 可以对build进行前置后置操作
+tasks.register<Task>("literalAssassin") {
+    description = "Project Assassin build.gradle task"
+    group = "assassin"
+    doFirst {
+        println("Assassin is running...")
+    }
+    doLast {
+        println("Assassin is done.")
+    }
+}
